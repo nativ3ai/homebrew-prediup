75251e3a995c52358afed31c7ebaa45c27f81082
NATIVE <native@NATIVEs-Mini.home>
2026-03-21 16:20:52 +0100

    feat: add predihermes formula

--- Formula/predihermes.rb
+++ Formula/predihermes.rb
@@ -1,7 +1,7 @@
-class Prediup < Formula
+class Predihermes < Formula
   include Language::Python::Virtualenv
 
-  desc "Compatibility formula for predihermes"
+  desc "One-command installer for the PrediHermes local edition"
   homepage "https://github.com/nativ3ai/prediup"
   url "https://github.com/nativ3ai/prediup/archive/refs/tags/v0.1.1.tar.gz"
   sha256 "67cb1f4408c8d2e66d889a2396dcd248a70abbc99870bfe47d11571e33502c26"
@@ -14,7 +14,7 @@ class Prediup < Formula
   end
 
   test do
-    output = shell_output("#{bin}/prediup --help")
+    output = shell_output("#{bin}/predihermes --help")
     assert_match "Installer for the PrediHermes local edition", output
   end
 end
--- README.md
+++ README.md
@@ -1,15 +1,21 @@
 # homebrew-prediup
 
-Homebrew tap for `prediup`.
+Homebrew tap for the PrediHermes local installer.
 
 ## Install
 
 ```bash
 brew tap nativ3ai/prediup
-brew install prediup
+brew install predihermes
 ```
 
 Or directly:
+
+```bash
+brew install nativ3ai/prediup/predihermes
+```
+
+Compatibility formula:
 
 ```bash
 brew install nativ3ai/prediup/prediup
